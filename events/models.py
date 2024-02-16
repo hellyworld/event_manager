80a7b664068e7146f3a1615fc73621c4221b0017
hellyworld <adrian.helerea@gmail.com>
2024-02-16 09:26:25 +0200

    new events app and models Event and Registration

---
+++ events/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Event(models.Model):
+    owner = models.ForeignKey(User, related_name='owned_events',
+                              on_delete=models.CASCADE)  # to keep the event (independent) I can use SET_NULL
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    max_attendees = models.PositiveIntegerField(default=10, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Registration(models.Model):
+    event = models.ForeignKey(Event, related_name='registrations', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='event_registrations', on_delete=models.CASCADE)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = [('event', 'user')]  # Prevents duplicate registrations
+
+    def __str__(self):
+        return f'{self.user.username} registered for {self.event.name}'
