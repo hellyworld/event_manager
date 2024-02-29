a133cd7c0632c14770989d2f4fdc96a9e9a45167
hellyworld <adrian.helerea@gmail.com>
2024-02-29 23:47:33 +0200

    Move schema and docs URLs under API namespace
    The URLs for the schema and docs views have been moved under the API namespace for better organization and easier navigation. This reduces confusion by clearly indicating that these particular paths correspond to the API.

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("api/", include("app_events.urls")),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("docs/", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
